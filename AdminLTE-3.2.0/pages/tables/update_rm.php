0f11e2ffdf869399b547dd362c54de5e049b4b51
Zaidan <zaidan.apf@gmail.com>
2024-03-04 22:36:46 +0800

    update rm

--- AdminLTE-3.2.0/pages/tables/update_rm.php
+++ AdminLTE-3.2.0/pages/tables/update_rm.php
@@ -1,5 +1,9 @@
 <?php
-    include 'config.php';
+include 'config.php';
+$id_rm = $_GET['id_rm'];
+$query = "SELECT * FROM rumahmakan WHERE id_rm = $id_rm";
+$result = mysqli_query($conn, $query);
+$brs= mysqli_fetch_array($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -220,67 +224,85 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Data Rumah Makan</h1>
+            <h1>Edit Data User</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Data Rumah Makan</li>
+              <li class="breadcrumb-item active">Edit Data User</li>
             </ol>
           </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
 
+
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-12">
-            <div class="card">
+          <!-- left column -->
+          <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Data RM Spin A Meal</h3>
+                <h3 class="card-title"></h3>
               </div>
               <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example2" class="table table-bordered">
-                  <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nama RM</th>
-                    <th>Alamat</th>
-                    <th>Lokasi</th>
-                    <th>No Telp</th>
-                    <th>Aksi</th>
-                  </tr>
-                  </thead>
-                  <?php
-                  $sql="SELECT * FROM rumahmakan";
-                  $query=mysqli_query($conn,$sql);
-                  while ($row = mysqli_fetch_assoc($query)) {
-                    echo "<tr>
-                    <td>" . $row['id_rm'] . "</td>
-                    <td>" . $row['nama_rumahmakan'] . "</td>
-                    <td>" . $row['Alamat'] . "</td>
-                    <td>" . $row['lokasi_rm'] . "</td>
-                    <td>" . $row['no_telp'] . "</td>
-                    <td>
-                        <a href='update_rm.php?id=" . $row['id_rm'] . "'>Edit</a> |
-                        <a href='delete_rm.php?id=" . $row['id_rm'] . "'>Delete</a>
-                    </td>
-                  </tr>";
+              <!-- form start -->
+              <form method="POST">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Nama Rumah Makan</label>
+                    <input type="text" class="form-control" name="nama_rumahmakan" placeholder="Rumah Makan">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Alamat</label>
+                    <input type="text" class="form-control" name="Alamat" placeholder="Alamat">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Lokasi</label>
+                    <input type="text" class="form-control" name="lokasi_rm" placeholder="Lokasi">
+                  </div>
+                  <label for="exampleInputPassword1">No Telepon</label>
+                    <input type="text" class="form-control" name="no_telp" placeholder="Nomor Telepon">
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" name="save" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+              <?php 
+              if(isset($_POST['save'])){
+                $nama_rumahmakan= $_POST['nama_rumahmakan'];
+                $Alamat = $_POST['Alamat'];
+                $lokasi_rm = $_POST['lokasi_rm'];
+                $no_telp = $_POST['no_telp'];
+                
+                $qry="UPDATE rumahmakan SET nama_rumahmakan= '$nama_rumahmakan',Alamat='$Alamat',lokasi_rm='$lokasi_rm',no_telp='$no_telp' WHERE id_rm=$id_rm";
+                $hasil = mysqli_query($conn,$qry);
+                if($hasil){
+                  echo "<script language='JavaScript'>
+                  (window.alert('Data Rumah Makan sudah di Update'))
+                  location.href='data_rm.php'
+                  </script>";
+                
+                }else{
+                  echo "<script language='JavaScript'>
+                  (window.alert('Data Rumah Makan tidak dapat di Update'))
+                  location.href='data_rm.php'
+                  </script>";
                 }
-                ?>
-                </table>
-              </div>
-              <!-- /.card-body -->
+              }
+              ?>
             </div>
+            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
         <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
+      </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
@@ -304,40 +326,17 @@
 <script src="../../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- bs-custom-file-input -->
+<script src="../../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
 <!-- Page specific script -->
 <script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
+$(function () {
+  bsCustomFileInput.init();
+});
 </script>
 </body>
 </html>
