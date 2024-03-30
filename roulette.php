a267267b18307200cd95ef68adc77f64ebf66df4
Varian719 <72840944+Varian719@users.noreply.github.com>
2024-03-30 23:21:48 +0800

    show menu

--- roulette.php
+++ roulette.php
@@ -390,62 +390,7 @@ error_reporting(0);
                     <td>" . $row['id_rm'] . "</td>
                     <td>" . $row['nama_rumahmakan'] . "</td>
                     <td>" . $row['Alamat'] . "</td>
-                    <td><button id='popup-btn'>Show Menu</button>
-                    <div id='popup' class='hidden'>
-                      <table id='table-data'></table>
-                    </div>
-                    <script type='javascript'>
-                    const sql = require('mssql');
-
-                    const config = {
-                      server: 'localhost', //server name or IP address
-                      database: 'spin_a_meal',
-                      user: 'root',
-                      password: '',
-                      options: {
-                        encrypt: true // use this if you're on Windows Azure
-                      }
-                    };
-                    
-                    sql.connect(config, (err) => {
-                      if (err) {
-                        console.error(err);
-                        return;
-                      }
-                    
-                      console.log('Connected to the database!');
-                    
-                      const popupBtn = document.getElementById('popup-btn');
-                      const popup = document.getElementById('popup');
-                      const tableData = document.getElementById('table-data');
-                    
-                      popupBtn.addEventListener('click', () => {
-                        // Define the SQL query to fetch the table data
-                        const query = 'SELECT * FROM menu';
-                    
-                        // Execute the query and get the result
-                        new sql.Request().query(query, (err, result) => {
-                          if (err) {
-                            console.error(err);
-                            return;
-                          }
-                    
-                          // Display the table data
-                          let html = '<table>';
-                          for (let row of result.recordset) {
-                            html += '<tr>';
-                            for (let cell of row) {
-                              html += `<td>${cell}</td>`;
-                            }
-                            html += '</tr>';
-                          }
-                          html += '</table>';
-                          tableData.innerHTML = html;
-                          popup.style.display = 'block';
-                        });
-                      });
-                    });
-                    </script>
+                    <td><a href='menu.php?id=". $row['id_rm'] ."' class='btn btn-primary'>Show Menu</a>
                   </tr>";
           }
           ?>
