d710f6e6caec7fa0d3b71245b4a6f0cbc09b4bd0
Armand Maulana <armandm792@outlook.com>
2024-05-22 20:09:02 +0800

    update design

--- index.php
+++ index.php
@@ -5,10 +5,17 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>HomePage</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+		integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+		integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+		crossorigin="anonymous"></script>
 	<style>
 		* {
 			padding: 0;
@@ -32,11 +39,26 @@
 			align-items: center;
 			justify-content: space-between;
 			margin: 0px;
-			background-color:#ad8edc;
+			background-color: #ad8edc;
 			/* Warna latar belakang navbar */
 			padding: 10px 20px;
-			
+
 			/* Menambahkan padding pada navbar */
+		}
+
+		#navbar .menu {
+			display: flex;
+			position: relative;
+		}
+
+		.menu #text {
+			text-decoration: none;
+			font-style: normal;
+			color: black;
+		}
+
+		.menu #text:hover {
+			color: #d0b5f2;
 		}
 
 		ul {
@@ -44,6 +66,7 @@
 			display: flex;
 			align-items: center;
 		}
+
 		ul span {
 			font-size: 24px;
 		}
@@ -82,7 +105,7 @@
 			display: flex;
 			justify-content: space-between;
 			flex-wrap: wrap;
-			background-color:#ad8edc;
+			background-color: #ad8edc;
 			padding: 20px;
 		}
 
@@ -107,61 +130,59 @@
 	<div class="container-1">
 
 		<div id="navbar">
-			<ul>
-				<img src="images/spin_a_meal.png" alt="Logo" class="logo">
-				<span>Spin A Meal</span>
-			</ul>
-			<ul>
-				<li><a href="login.php">Login</a></li>
-				<li><a href="dashboard_guest.php">Login As Guest</a></li>
-			</ul>
-
+			<div class="logo">
+				<ul>
+					<img src="images/spin_a_meal.png" alt="Logo" class="logo">
+					<span>Spin A Meal</span>
+				</ul>
+			</div>
+			<div class="menu">
+				<ul>
+					<li><a id="text" href="dashboard_guest.php">Try</a></li>
+				</ul>
+				<ul>
+					<li><a id="text" href="login.php">Login</a></li>
+				</ul>
+			</div>
 		</div>
 		<div class="content">
 			<div class="box">
-			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="images/img1.png" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images/img2.png" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="images/img3.png" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+				<H1> Best Rated Restaurant</h1>
+			</div>
+			<div class="box">
+				<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="images/img1.png" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="images/img2.png" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="images/img3.png" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+
 		</div>
-		<div class="box">
-			<H1> Best Rated Restaurant</h1>
-	</div>
-	</div>
-	<div class="content">
+		<div class="content">
 			<div class="box">
 				<h3>This Website is a random picker for users who want to try something new to eat</h3>
 			</div>
 			<div class="box">
 				<img src="images/undraw_eating_together_re_ux62.svg" alt="Illustration">
 			</div>
-	</div>
-	<div class="content">
-			<div class="box">
-			<img src="images/img4.png">
-				
-			</div>
-			<div class="box">
-				<h3>Tampilan Spin A Meal Roulette<h3>
-			</div>
-	</div>
+		</div>
+
 		<div id="footer" class="footer">
 			<div class="box">
 				<h4>Latest News</h4>
